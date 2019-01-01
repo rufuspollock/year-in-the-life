90d29e2569a2763e4e587653a75361eb17344e69
Rufus Pollock <rufus.pollock@okfn.org>
2019-01-01 19:00:53 +0100

    [viz.js][s]: (old: ~1y) small refactor to show 2 calendars.
    * Also add sample data for sylvie

--- src/viz.js
+++ src/viz.js
@@ -111,21 +111,34 @@ function draw(chartData, startDate, endDate, selector='.container') {
 
 var startDate = moment.utc('2018-01-01');
 var endDate = moment.utc('2019-01-01');
-var chartData = {};
-d3.utcDays(startDate, endDate).map(function (dateElement) {
-  dateElement = moment.utc(dateElement);
-  var color = '#d9d9d9';
-  // sundays are reserved
-  if (dateElement.isoWeekday() == 7) {
-    color = 'pink'
-  }
-  chartData[moment(dateElement).toISOString().slice(0,10)] = {
-    color: color,
-    title: ''
-  };
-});
-d3.csv('events-2018-rufus.csv', function(error, csv) {
+
+d3.csv('data/events-2018-rufus.csv', function(error, csv) {
   if (error) throw error;
+  var chartData = processCsvRows(csv);
+  draw(chartData, startDate, endDate);
+})
+
+d3.csv('data/events-2018-sylvie.csv', function(error, csv) {
+  if (error) throw error;
+  var chartData = processCsvRows(csv);
+  draw(chartData, startDate, endDate);
+})
+
+// process csv rows to add colors etc based on location, activity etc
+function processCsvRows(csv) {
+  var chartData = {};
+  d3.utcDays(startDate, endDate).map(function (dateElement) {
+    dateElement = moment.utc(dateElement);
+    var color = '#d9d9d9';
+    // sundays are reserved
+    if (dateElement.isoWeekday() == 7) {
+      color = 'pink'
+    }
+    chartData[moment(dateElement).toISOString().slice(0,10)] = {
+      color: color,
+      title: ''
+    };
+  });
 
   for(let row of csv) {
     var day = moment.utc(row.start);
@@ -146,9 +159,8 @@ d3.csv('events-2018-rufus.csv', function(error, csv) {
       day = day.add(1, 'day')
     }
   }
-
-  draw(chartData, startDate, endDate);
-})
+  return chartData;
+}
 
 /*
 function duringAnEvent(date) {
